a7212e546ef7c2d2f523f0cb767c074d2471cb9d
ryosuke_sato <r-sato@feedforce.jp>
2019-05-22 18:31:35 +0900

    Wrap with a factory class

--- lib/my_api_client/request.rb
+++ lib/my_api_client/request.rb
@@ -2,8 +2,6 @@
 
 module MyApiClient
   module Request
-    include MyApiClient::Exceptions
-
     %i[get post patch delete].each do |http_method|
       class_eval <<~METHOD, __FILE__, __LINE__ + 1
         def #{http_method}(url, headers: nil, query: nil, body: nil)
@@ -15,36 +13,19 @@ module MyApiClient
 
     private
 
-    def faraday
-      Faraday.new(nil, request: request_option)
-    end
-
-    def request_option
-      @request_option ||= {
-        timeout: respond_to?(:request_timeout) ? request_timeout : nil,
-        open_timeout: respond_to?(:request_open_timeout) ? request_open_timeout : nil
-      }.compact
+    def core
+      @core ||= RequestCore.new(endpoint, error_handlers, faraday_options)
     end
 
     def request(method, url, headers, query, body)
-      # TODO: Wrap with a factory class
-      logger = Logger.new(faraday, method, url)
-      agent = Sawyer::Agent.new(endpoint, faraday: faraday)
-      agent.call(method, url, body, headers: headers, query: query).tap do |response|
-        logger.info("Duration #{response.timing} sec")
-        params = [] # TODO: create a params
-        verify(params, logger)
-      end.data
+      core.request(method, url, headers, query, body)
     end
 
-    def verify(params, logger)
-      error_handler = error_handlers.detect { |h| h.call(params.response) }
-      case error_handler
-      when Proc
-        error_handler.call(params, logger)
-      when Symbol
-        send(error_handler, params, logger)
-      end
+    def faraday_options
+      @faraday_options ||= {
+        timeout: (request_timeout if respond_to?(:request_timeout)),
+        open_timeout: (net_open_timeout if respond_to?(:net_open_timeout))
+      }.compact
     end
   end
 end
---
+++ lib/my_api_client/request_core.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module MyApiClient
+  class RequestCore
+    include MyApiClient::Exceptions
+
+    def initialize(endpoint, error_handlers, options)
+      @error_handlers = error_handlers
+      @faraday = Faraday.new(nil, request: options)
+      @agent = Sawyer::Agent.new(endpoint, faraday: faraday)
+    end
+
+    def request(method, url, headers, query, body)
+      @logger = Logger.new(faraday, method, url)
+      call(:execute, method, url, headers, query, body)
+    end
+
+    private
+
+    attr_reader :error_handlers, :faraday, :agent, :logger
+
+    def execute(method, url, headers, query, body)
+      response = agent.call(method, url, body, headers: headers, query: query)
+      logger.info("Duration #{response.timing} sec")
+      params = [] # TODO: create a params
+      verify(params)
+      response.data
+    end
+
+    def verify(params)
+      error_handler = error_handlers.detect { |h| h.call(params.response) }
+      case error_handler
+      when Proc
+        error_handler.call(params, logger)
+      when Symbol
+        send(error_handler, params, logger)
+      end
+    end
+  end
+end
