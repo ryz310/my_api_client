b4260811a072e51ad6c5231dca1b0a92c540cbc4
ryosuke_sato <r-sato@feedforce.jp>
2019-05-22 17:43:32 +0900

    Refactoring

--- lib/my_api_client/request.rb
+++ lib/my_api_client/request.rb
@@ -4,28 +4,6 @@ module MyApiClient
   module Request
     include MyApiClient::Exceptions
 
-    def request(method, url, headers, query, body)
-      # TODO: Wrap with a factory class
-      logger = Logger.new(faraday, method, url)
-      agent = Sawyer::Agent.new(endpoint, faraday: faraday)
-      agent.call(method, url, body, headers: headers, query: query).tap do |response|
-        logger.info("Duration #{response.timing} sec")
-        params = [] # TODO: create a params
-        verify(params, logger)
-      end.data
-    end
-
-    def verify(params, logger)
-      error_handler = error_handlers.find { |h| h.call(params.response) }
-
-      case error_handler
-      when Proc
-        error_handler.call(params, logger)
-      when Symbol
-        send(error_handler, params, logger)
-      end
-    end
-
     %i[get post patch delete].each do |http_method|
       class_eval <<~METHOD, __FILE__, __LINE__ + 1
         def #{http_method}(url, headers: nil, query: nil, body: nil)
@@ -46,6 +24,27 @@ module MyApiClient
         timeout: respond_to?(:request_timeout) ? request_timeout : nil,
         open_timeout: respond_to?(:request_open_timeout) ? request_open_timeout : nil
       }.compact
+    end
+
+    def request(method, url, headers, query, body)
+      # TODO: Wrap with a factory class
+      logger = Logger.new(faraday, method, url)
+      agent = Sawyer::Agent.new(endpoint, faraday: faraday)
+      agent.call(method, url, body, headers: headers, query: query).tap do |response|
+        logger.info("Duration #{response.timing} sec")
+        params = [] # TODO: create a params
+        verify(params, logger)
+      end.data
+    end
+
+    def verify(params, logger)
+      error_handler = error_handlers.detect { |h| h.call(params.response) }
+      case error_handler
+      when Proc
+        error_handler.call(params, logger)
+      when Symbol
+        send(error_handler, params, logger)
+      end
     end
   end
 end
