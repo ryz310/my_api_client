490e13edab242a03a6c5de6b742c9d891a947af0
ryz310 <ryz310@gmail.com>
2019-05-25 02:10:57 +0900

    Implement the request module

--- lib/my_api_client/request.rb
+++ lib/my_api_client/request.rb
@@ -2,9 +2,17 @@
 
 module MyApiClient
   module Request
-    def request(method, url, headers, query, body)
-      request_params = Params::Request.new(method, url, headers, query, body)
-      logger = Logger.new(faraday, method, url)
+    # Description of #request
+    #
+    # @param http_method [Symbol] describe_http_method_here
+    # @param url [String] describe_url_here
+    # @param headers [Hash, nil] describe_headers_here
+    # @param query [Hash, nil] describe_query_here
+    # @param body [Hash, nil] describe_body_here
+    # @return [Sawyer::Resource] description_of_returned_object
+    def request(http_method, url, headers, query, body)
+      request_params = Params::Request.new(http_method, url, headers, query, body)
+      logger = Logger.new(faraday, http_method, url)
       call(:execute, request_params, logger)
     end
 
@@ -26,14 +34,20 @@ module MyApiClient
     end
 
     def execute(request_params, logger)
+      logger.info('Start')
       response = agent.call(*request_params.to_sawyer_args)
       logger.info("Duration #{response.timing} sec")
       params = Params::Params.new(request_params, response)
-      verify(params)
+      verify(params, logger)
+    rescue MyApiClient::Error => e
+      logger.warn("Failure (#{response.status})")
+      raise e
+    else
+      logger.info("Success (#{response.status})")
       response.data
     end
 
-    def verify(params)
+    def verify(params, logger)
       case error_handler = error_handling(params.response)
       when Proc
         error_handler.call(params, logger)
