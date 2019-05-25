230f0ec67844edf337a769f30f132bcf42104e2f
ryz310 <ryz310@gmail.com>
2019-05-25 23:00:12 +0900

    Use built-in error classes

--- spec/dummy_app/api_clients/application_api_client.rb
+++ spec/dummy_app/api_clients/application_api_client.rb
@@ -6,13 +6,10 @@ class ApplicationApiClient < MyApiClient::Base
   request_timeout 3.seconds
   net_open_timeout 2.seconds
 
-  class IgnorableError < MyApiClient::Error; end
-
   retry_on_network_errors wait: 0.1.seconds do |_client, error, logger|
     logger.warn error.message
   end
 
-  error_handling status_code: 500..999 do |params|
-    raise IgnorableError, params
-  end
+  error_handling status_code: 400..499, raise: MyApiClient::ClientError
+  error_handling status_code: 500..599, raise: MyApiClient::ServerError
 end
