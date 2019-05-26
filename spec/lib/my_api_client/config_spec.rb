27d4359b4f77b16eda20480e5f28e473249ed732
ryz310 <ryz310@gmail.com>
2019-05-26 12:08:24 +0900

    Fix how to define class in the rspec

--- spec/lib/my_api_client/config_spec.rb
+++ spec/lib/my_api_client/config_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe MyApiClient::Config do
-  class MockClass
+  class self::MockClass
     include MyApiClient::Config
   end
-  let(:instance) { MockClass.new }
+  let(:instance) { self.class::MockClass.new }
 
   described_class::CONFIG_METHODS.each do |config_method|
     describe "##{config_method}" do
-      before { MockClass.public_send(config_method, 'value') }
+      before { self.class::MockClass.public_send(config_method, 'value') }
 
       it 'returns a value which set from the class method' do
         expect(instance.public_send(config_method)).to eq 'value'
