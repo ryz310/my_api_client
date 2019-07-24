6528fdb7bc9e87be7fec81b80dd315c02f0b2252
ryosuke_sato <r-sato@feedforce.jp>
2019-07-24 20:04:35 +0900

    Add the error handler generator class

--- lib/my_api_client/error_handling.rb
+++ lib/my_api_client/error_handling.rb
@@ -32,53 +32,19 @@ module MyApiClient
       #   Raises specified error when error detected. default: MyApiClient::Error
       # @param block [Proc]
       #   Executes the block when error detected
-      def error_handling(status_code: nil, json: nil, with: nil, raise: MyApiClient::Error)
+      def error_handling(status_code: nil, json: nil, with: nil, raise: MyApiClient::Error, &block)
         temp = error_handlers.dup
         temp << lambda { |response|
-          if match?(status_code, response.status) && match_all?(json, response.body)
-            if block_given?
-              ->(params, logger) { yield params, logger }
-            elsif with
-              with
-            else
-              ->(params, _logger) { raise raise, params }
-            end
-          end
+          Generator.call(
+            response: response,
+            status_code: status_code,
+            json: json,
+            with: with,
+            raise: raise,
+            block: (block if block_given?)
+          )
         }
         self.error_handlers = temp
-      end
-
-      private
-
-      # rubocop:disable Metrics/CyclomaticComplexity
-      def match?(operator, target)
-        case operator
-        when nil
-          true
-        when String, Integer, TrueClass, FalseClass
-          operator == target
-        when Range
-          operator.include?(target)
-        when Regexp
-          operator =~ target.to_s
-        when Symbol
-          target.respond_to?(operator) && target.public_send(operator)
-        else
-          raise "Unexpected operator type was given: #{operator.inspect}"
-        end
-      end
-      # rubocop:enable Metrics/CyclomaticComplexity
-
-      def match_all?(json, response_body)
-        return true if json.nil?
-        return false if response_body.blank?
-
-        json.all? do |path, operator|
-          target = JsonPath.new(path.to_s).first(response_body)
-          match?(operator, target)
-        end
-      rescue MultiJson::ParseError
-        false
       end
     end
 
