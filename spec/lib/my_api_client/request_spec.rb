807cc0eca2b075749175e1cb270ec27447b9ff5a
ryz310 <ryz310@gmail.com>
2019-10-13 15:55:30 +0900

    Add specifications for other HTTP methods

--- spec/lib/my_api_client/request_spec.rb
+++ spec/lib/my_api_client/request_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe MyApiClient::Request do
 
   describe '#_request' do
     subject(:request!) do
-      instance._request(:get, '/path/to/resource', headers, query, body, logger)
+      instance._request(http_method, '/path/to/resource', headers, query, body, logger)
     end
 
     before do
@@ -37,115 +37,159 @@ RSpec.describe MyApiClient::Request do
       allow(Sawyer::Agent).to receive(:new).and_return(agent)
       allow(Faraday).to receive(:new).and_call_original
       allow(instance).to receive(:error_handling).and_call_original
-      stub_request(:get, 'https://example.com/v1/path/to/resource')
+      stub_request(http_method, 'https://example.com/v1/path/to/resource')
         .with(query: query)
         .to_return(body: response_body, headers: headers)
     end
 
     let(:instance) { self.class::MockClass.new }
     let(:headers) { { 'Content-Type': 'application/json;charset=UTF-8' } }
-    let(:query) { { key: 'value' } }
-    let(:body) { nil }
-    let(:response_body) { { message: 'OK' }.to_json }
+    let(:request_logger) { instance_double(MyApiClient::Logger, info: nil, warn: nil, error: nil) }
     let(:agent) { instance_double(Sawyer::Agent, call: response) }
     let(:response) do
       instance_double(Sawyer::Response, status: 200, data: resource, timing: 0.1, headers: nil)
     end
     let(:resource) { instance_double(Sawyer::Resource) }
     let(:logger) { instance_double(::Logger) }
-    let(:request_logger) { instance_double(MyApiClient::Logger, info: nil, warn: nil, error: nil) }
+    let(:response_body) { { message: 'OK' }.to_json }
 
-    it 'builds a request parameter instance with arguments' do
-      request!
-      expect(MyApiClient::Params::Request)
-        .to have_received(:new).with(:get, '/v1/path/to/resource', headers, query, body)
-    end
+    shared_examples 'to initialize an instance of each class' do
+      it 'builds a request parameter instance with arguments' do
+        request!
+        expect(MyApiClient::Params::Request)
+          .to have_received(:new).with(http_method, '/v1/path/to/resource', headers, query, body)
+      end
 
-    it 'builds a request logger instance with arguments' do
-      request!
-      expect(MyApiClient::Logger)
-        .to have_received(:new)
-        .with(logger, instance_of(Faraday::Connection), :get, '/v1/path/to/resource')
-    end
+      it 'builds a request logger instance with arguments' do
+        request!
+        expect(MyApiClient::Logger)
+          .to have_received(:new)
+          .with(logger, instance_of(Faraday::Connection), http_method, '/v1/path/to/resource')
+      end
 
-    it 'builds a Sawyer::Agent instance with the configuration parameter' do
-      request!
-      expect(Sawyer::Agent)
-        .to have_received(:new)
-        .with('https://example.com', faraday: instance_of(Faraday::Connection))
-    end
+      it 'builds a Sawyer::Agent instance with the configuration parameter' do
+        request!
+        expect(Sawyer::Agent)
+          .to have_received(:new)
+          .with('https://example.com', faraday: instance_of(Faraday::Connection))
+      end
 
-    it 'builds a Faraday instance with configuration parameters' do
-      request!
-      expect(Faraday)
-        .to have_received(:new)
-        .with(nil, request: { timeout: 3.seconds, open_timeout: 2.seconds })
-    end
+      it 'builds a Faraday instance with configuration parameters' do
+        request!
+        expect(Faraday)
+          .to have_received(:new)
+          .with(nil, request: { timeout: 3.seconds, open_timeout: 2.seconds })
+      end
 
-    it 'initializes in order of faraday, sawyer, logger' do
-      request!
-      expect(Faraday).to have_received(:new).ordered
-      expect(Sawyer::Agent).to have_received(:new).ordered
-      expect(MyApiClient::Logger).to have_received(:new).ordered
-    end
+      it 'builds the Params instance with request and response parameters' do
+        request!
+        expect(MyApiClient::Params::Params)
+          .to have_received(:new)
+          .with(instance_of(MyApiClient::Params::Request), response)
+      end
 
-    it 'calls Sawyer::Agent#call with request parameters' do
-      request!
-      expect(agent)
-        .to have_received(:call)
-        .with(:get, '/v1/path/to/resource', body, headers: headers, query: query)
-    end
-
-    it 'builds the Params instance with request and response parameters' do
-      request!
-      expect(MyApiClient::Params::Params)
-        .to have_received(:new)
-        .with(instance_of(MyApiClient::Params::Request), response)
-    end
-
-    it 'calls #error_handling with response parameters' do
-      request!
-      expect(instance).to have_received(:error_handling).with(response)
-    end
-
-    it 'returns HTTP request response' do
-      expect(request!).to eq resource
-    end
-
-    context 'when #error_handling returns Proc' do
-      before { allow(instance).to receive(:error_handling).and_return(proc) }
-
-      let(:proc) { ->(_params, _request_logger) { puts 'The procedure is called' } }
-
-      it 'calls received procedure' do
-        expect { request! }.to output("The procedure is called\n").to_stdout
+      it 'initializes in order of faraday, sawyer, logger' do
+        request!
+        expect(Faraday).to have_received(:new).ordered
+        expect(Sawyer::Agent).to have_received(:new).ordered
+        expect(MyApiClient::Logger).to have_received(:new).ordered
       end
     end
 
-    context 'when #error_handling returns Symbol' do
-      before { allow(instance).to receive(:error_handling).and_return(:bad_request) }
+    shared_examples 'to execute an HTTP request' do
+      it 'requests to the API by Sawyer::Agent#call' do
+        request!
+        expect(agent)
+          .to have_received(:call)
+          .with(http_method, '/v1/path/to/resource', body, headers: headers, query: query)
+      end
 
-      it 'executes received Symbol\'s method' do
-        expect { request! }.to output("The method is called\n").to_stdout
+      it 'verifies the API response with `#error_handling` definition' do
+        request!
+        expect(instance).to have_received(:error_handling).with(response)
+      end
+
+      it 'returns the API response' do
+        expect(request!).to eq resource
       end
     end
 
-    context 'when detects some network error' do
-      before { allow(agent).to receive(:call).and_raise(Net::OpenTimeout) }
+    shared_examples 'to handle errors' do
+      context 'when #error_handling returns Proc' do
+        before { allow(instance).to receive(:error_handling).and_return(proc) }
 
-      it 'raises MyApiClient::NetworkError' do
-        expect { request! }.to raise_error(MyApiClient::NetworkError)
+        let(:proc) { ->(_params, _request_logger) { puts 'The procedure is called' } }
+
+        it 'calls received procedure' do
+          expect { request! }.to output("The procedure is called\n").to_stdout
+        end
+      end
+
+      context 'when #error_handling returns Symbol' do
+        before { allow(instance).to receive(:error_handling).and_return(:bad_request) }
+
+        it 'executes received Symbol\'s method' do
+          expect { request! }.to output("The method is called\n").to_stdout
+        end
+      end
+
+      context 'when detects some network error' do
+        before { allow(agent).to receive(:call).and_raise(Net::OpenTimeout) }
+
+        it 'raises MyApiClient::NetworkError' do
+          expect { request! }.to raise_error(MyApiClient::NetworkError)
+        end
+      end
+
+      context 'when raises a error which inherit MyApiClient::Error' do
+        before { allow(instance).to receive(:error_handling).and_return(proc) }
+
+        let(:proc) { ->(params, _request_logger) { raise MyApiClient::Error, params } }
+
+        it 'escalates the error' do
+          expect { request! }.to raise_error(MyApiClient::Error)
+        end
       end
     end
 
-    context 'when raises a error which inherit MyApiClient::Error' do
-      before { allow(instance).to receive(:error_handling).and_return(proc) }
+    context 'when requesting with GET method' do
+      let(:http_method) { :get }
+      let(:query) { { key: 'value' } }
+      let(:body) { nil }
 
-      let(:proc) { ->(params, _request_logger) { raise MyApiClient::Error, params } }
+      it_behaves_like 'to initialize an instance of each class'
+      it_behaves_like 'to execute an HTTP request'
+      it_behaves_like 'to handle errors'
+    end
 
-      it 'escalates the error' do
-        expect { request! }.to raise_error(MyApiClient::Error)
-      end
+    context 'when requesting with POST method' do
+      let(:http_method) { :post }
+      let(:query) { nil }
+      let(:body) { { name: 'name', birthday: Date.new(1999, 1, 1) } }
+
+      it_behaves_like 'to initialize an instance of each class'
+      it_behaves_like 'to execute an HTTP request'
+      it_behaves_like 'to handle errors'
+    end
+
+    context 'when requesting with PATCH method' do
+      let(:http_method) { :patch }
+      let(:query) { nil }
+      let(:body) { { name: 'name', birthday: Date.new(1999, 1, 1) } }
+
+      it_behaves_like 'to initialize an instance of each class'
+      it_behaves_like 'to execute an HTTP request'
+      it_behaves_like 'to handle errors'
+    end
+
+    context 'when requesting with DELETE method' do
+      let(:http_method) { :delete }
+      let(:query) { nil }
+      let(:body) { nil }
+
+      it_behaves_like 'to initialize an instance of each class'
+      it_behaves_like 'to execute an HTTP request'
+      it_behaves_like 'to handle errors'
     end
   end
 end
