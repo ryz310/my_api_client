7e43fbde0dfecc9ca27fd263244cb1b8ddd0a934
ryz310 <ryz310@gmail.com>
2019-10-22 17:07:28 +0900

    Modify the matcher

--- lib/my_api_client/rspec/matchers/be_handled_as_an_error.rb
+++ lib/my_api_client/rspec/matchers/be_handled_as_an_error.rb
@@ -7,28 +7,51 @@ RSpec::Matchers.define :be_handled_as_an_error do |expected_error_class|
   include MyApiClient::MatcherHelper
 
   match do |api_request|
-    begin
-      api_request.call
-    rescue StandardError => e
-      if expected_error_class
-        e.is_a? expected_error_class
-      else
-        true
-      end
+    init
+    handle_error(api_request).is_a? expected_error_class
+  end
+
+  match_when_negated do |api_request|
+    init
+    handle_error(api_request).nil?
+  end
+
+  chain :when_receive, :expected_response
+
+  failure_message do |api_request|
+    actual_error = handle_error(api_request)
+    if actual_error.nil?
+      "expected to be handled as #{expected_error_class.name}, " \
+      'but not to be handled'
     else
-      false
+      "expected to be handled as #{expected_error_class.name}, " \
+      "but it was handled as #{actual_error.class.name}"
     end
   end
 
-  chain :when_receive do |api_response|
+  failure_message_when_negated do |api_request|
+    actual_error = handle_error(api_request)
+    'expected not to be handled as an error, ' \
+    "but it was handled as #{actual_error.class.name}"
+  end
+
+  def init
     disable_logging
     response = dummy_response(
-      status: api_response[:status_code] || 200,
-      headers: api_response[:headers] || {},
-      body: api_response[:body] || nil
+      status: expected_response[:status_code] || 200,
+      headers: expected_response[:headers] || {},
+      body: expected_response[:body] || nil
     )
     sawyer = instance_double(Sawyer::Agent, call: response)
     allow(Sawyer::Agent).to receive(:new).and_return(sawyer)
+  end
+
+  def handle_error(api_request)
+    api_request.call
+  rescue MyApiClient::Error => e
+    e
+  else
+    nil
   end
 
   supports_block_expectations
