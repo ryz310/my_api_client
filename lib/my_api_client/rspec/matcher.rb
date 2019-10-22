d8d379c3c6ba163b83f5a6abe524ab78517eba4f
ryz310 <ryz310@gmail.com>
2019-10-22 13:53:14 +0900

    Refactoring

--- lib/my_api_client/rspec/matcher.rb
+++ lib/my_api_client/rspec/matcher.rb
@@ -15,27 +15,33 @@ RSpec::Matchers.define :have_requested_to do |expected_method, expected_url|
       headers: {},
       body: nil
     )
+    @expected = {
+      method: expected_method,
+      url: expected_url,
+      body: expected_options[:body],
+      headers: expected_options[:headers],
+      query: expected_options[:query],
+    }.compact
+    @actual = {}
     sawyer = instance_double(Sawyer::Agent)
-    result = true
-    allow(sawyer).to receive(:call) do |method, pathname, body, options|
-      result =
-        (expected_method == method) &&
-        (@expected_pathname == pathname) &&
-        (@expected_body == body) &&
-        (@expected_headers == options[:headers]) &&
-        (@expected_query == options[:query])
-    end.and_return(response)
     allow(Sawyer::Agent).to receive(:new) do |schema_and_hostname|
-      @expected_pathname = expected_url.sub(schema_and_hostname, '')
+      @actual_schema_and_hostname = schema_and_hostname
     end.and_return(sawyer)
+    allow(sawyer).to receive(:call) do |method, pathname, body, options|
+      @actual =
+        {
+          method: method,
+          url: @actual_schema_and_hostname + pathname,
+          body: body,
+          headers: options[:headers],
+          query: options[:query],
+        }.compact
+    end.and_return(response)
     api_request.call
-    result
+    @expected == @actual
   end
 
-  chain :with do |expected_options|
-    @expected_body = expected_options[:body]
-    @expected_headers = expected_options[:headers]
-    @expected_query = expected_options[:query]
+  chain :with, :expected_options
   end
 
   supports_block_expectations
