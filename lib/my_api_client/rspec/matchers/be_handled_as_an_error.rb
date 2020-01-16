7bf31516832bde94073a426c69036a806caea7d7
ryz310 <ryz310@gmail.com>
2020-01-17 01:17:08 +0900

    Change coding style

--- lib/my_api_client/rspec/matchers/be_handled_as_an_error.rb
+++ lib/my_api_client/rspec/matchers/be_handled_as_an_error.rb
@@ -16,9 +16,9 @@ RSpec::Matchers.define :be_handled_as_an_error do |expected_error_class|
     handle_error(api_request).nil?
   end
 
-  chain :after_retry, :retry_count
-  chain(:times) { nil }
-  chain :when_receive, :expected_response
+  chain(:after_retry, :retry_count)
+  chain(:times) {}
+  chain(:when_receive, :expected_response)
 
   description do
     message = "be handled as #{expected_error_class || 'an error'}"
