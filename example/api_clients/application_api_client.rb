2da037dcb9bf42105d185cce7edea9d3cd995c1e
ryz310 <ryz310@gmail.com>
2020-03-01 17:43:49 +0900

    Add an integration test

---
+++ example/api_clients/application_api_client.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# An usage example of the `my_api_client`.
+class ApplicationApiClient < MyApiClient::Base
+  endpoint ENV['MY_API_ENDPOINT']
+
+  self.logger = ::Logger.new(nil)
+
+  http_open_timeout 2.seconds
+  http_read_timeout 3.seconds
+
+  retry_on MyApiClient::NetworkError, wait: 0.seconds, attempts: 3
+
+  error_handling status_code: 400..499, raise: MyApiClient::ClientError
+  error_handling status_code: 500..599 do |params, logger|
+    logger.warn 'Server error occurred.'
+    raise MyApiClient::ServerError, params
+  end
+end
---
+++ spec/integrations/api_clients/my_rest_api_client_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require './example/api_clients/my_rest_api_client'
+
+RSpec.describe 'Integration test with My REST API' do
+  before do
+    WebMock.disable_net_connect!(allow: /#{ENV['MY_API_ENDPOINT']}*/)
+  end
+
+  after do
+    WebMock.disable_net_connect!
+  end
+
+  let(:api_client) { MyRestApiClient.new }
+  let(:id) { rand(100) }
+
+  describe 'GET rest' do
+    it 'returns array of ID' do
+      response = api_client.get_all_id
+      expect(response[0].id).to eq 1
+      expect(response[1].id).to eq 2
+      expect(response[2].id).to eq 3
+    end
+  end
+
+  describe 'GET rest/:id' do
+    it 'returns specified ID and message' do
+      response = api_client.get_id(id: id)
+      expect(response.id).to eq id
+      expect(response.message).to eq 'The resource is readed.'
+    end
+  end
+
+  describe 'POST rest' do
+    it 'returns ID and message' do
+      response = api_client.post_id
+      expect(response.id).to eq 1
+      expect(response.message).to eq 'The resource is created.'
+    end
+  end
+
+  describe 'POST/PUT/PATCH rest/:id' do
+    it 'returns specified ID and message' do
+      response = api_client.patch_id(id: id)
+      expect(response.id).to eq id
+      expect(response.message).to eq 'The resource is updated.'
+    end
+  end
+
+  describe 'DELETE rest/:id' do
+    it 'returns no contents' do
+      response = api_client.delete_id(id: id)
+      expect(response).to be_nil
+    end
+  end
+end
