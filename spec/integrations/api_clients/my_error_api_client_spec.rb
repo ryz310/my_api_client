b2f942513374b97da0bc5571ca0ae1c4eef7ea42
ryz310 <ryz310@gmail.com>
2020-03-05 23:01:59 +0900

    Implement MyErrorApiClient and add specifications

---
+++ spec/integrations/api_clients/my_error_api_client_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require './example/api_clients/my_error_api_client'
+
+RSpec.describe 'Integration test with My Error API' do
+  before do
+    WebMock.disable_net_connect!(allow: /#{ENV['MY_API_ENDPOINT']}*/)
+  end
+
+  after do
+    WebMock.disable_net_connect!
+  end
+
+  let(:api_client) { MyErrorApiClient.new }
+
+  describe 'GET error/:code' do
+    context 'with error code: 0' do
+      it do
+        expect { api_client.get_error(code: 0) }
+          .to raise_error(MyErrors::ErrorCode00)
+      end
+    end
+
+    context 'with error code: 10' do
+      it do
+        expect { api_client.get_error(code: 10) }
+          .to raise_error(MyErrors::ErrorCode10)
+      end
+    end
+
+    context 'with error code: 20 to 29' do
+      it do
+        expect { api_client.get_error(code: rand(20..29)) }
+          .to raise_error(MyErrors::ErrorCode2x)
+      end
+    end
+
+    context 'with error code: 30' do
+      it do
+        expect { api_client.get_error(code: 30) }
+          .to raise_error(MyErrors::ErrorCode30)
+      end
+    end
+
+    context 'with error code: other' do
+      it do
+        expect { api_client.get_error(code: 40) }
+          .to raise_error(MyErrors::ErrorCodeOther)
+      end
+    end
+  end
+end
