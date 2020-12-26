edb92480fe05bb0f0f5b8c482df879e8bc27e5df
ryz310 <ryz310@gmail.com>
2020-12-27 02:06:56 +0900

    Install RSpec

--- rails_app/rails_6.1/config/application.rb
+++ rails_app/rails_6.1/config/application.rb
@@ -13,7 +13,7 @@ require "action_controller/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
