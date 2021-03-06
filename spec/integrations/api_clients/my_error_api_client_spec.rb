e228a6be45e83839df3741749527c8b9991688e9
Ryosuke Sato <ryz310@gmail.com>
2021-03-07 01:35:01 +0900

    Add specifications using stubbed API client

--- spec/integrations/api_clients/my_error_api_client_spec.rb
+++ spec/integrations/api_clients/my_error_api_client_spec.rb
@@ -1,44 +1,71 @@
 # frozen_string_literal: true
 
 require './example/api_clients/my_error_api_client'
+require 'my_api_client/rspec'
 
 RSpec.describe 'Integration test with My Error API', type: :integration do
-  let(:api_client) { MyErrorApiClient.new }
-
   describe 'GET error/:code' do
-    context 'with error code: 0' do
-      it do
-        expect { api_client.get_error(code: 0) }
-          .to raise_error(MyErrors::ErrorCode00)
+    shared_examples 'the API client' do
+      context 'with error code: 0' do
+        it do
+          expect { api_client.get_error(code: 0) }
+            .to raise_error(MyErrors::ErrorCode00)
+        end
+      end
+
+      context 'with error code: 10' do
+        it do
+          expect { api_client.get_error(code: 10) }
+            .to raise_error(MyErrors::ErrorCode10)
+        end
+      end
+
+      context 'with error code: 20 to 29' do
+        it do
+          expect { api_client.get_error(code: rand(20..29)) }
+            .to raise_error(MyErrors::ErrorCode2x)
+        end
+      end
+
+      context 'with error code: 30' do
+        it do
+          expect { api_client.get_error(code: 30) }
+            .to raise_error(MyErrors::ErrorCode30)
+        end
+      end
+
+      context 'with error code: other' do
+        it do
+          expect { api_client.get_error(code: 40) }
+            .to raise_error(MyErrors::ErrorCodeOther)
+        end
       end
     end
 
-    context 'with error code: 10' do
-      it do
-        expect { api_client.get_error(code: 10) }
-          .to raise_error(MyErrors::ErrorCode10)
-      end
+    context 'with real connection' do
+      let(:api_client) { MyErrorApiClient.new }
+
+      it_behaves_like 'the API client'
     end
 
-    context 'with error code: 20 to 29' do
-      it do
-        expect { api_client.get_error(code: rand(20..29)) }
-          .to raise_error(MyErrors::ErrorCode2x)
-      end
-    end
+    context 'with stubbed API client' do
+      let(:api_client) { stub_api_client(MyErrorApiClient, get_error: get_error) }
 
-    context 'with error code: 30' do
-      it do
-        expect { api_client.get_error(code: 30) }
-          .to raise_error(MyErrors::ErrorCode30)
+      let(:get_error) do
+        lambda do |params|
+          exception =
+            case params[:code]
+            when 0      then MyErrors::ErrorCode00
+            when 10     then MyErrors::ErrorCode10
+            when 20..29 then MyErrors::ErrorCode2x
+            when 30     then MyErrors::ErrorCode30
+            else;            MyErrors::ErrorCodeOther
+            end
+          raise exception, MyApiClient::Params::Params.new(nil, nil)
+        end
       end
-    end
 
-    context 'with error code: other' do
-      it do
-        expect { api_client.get_error(code: 40) }
-          .to raise_error(MyErrors::ErrorCodeOther)
-      end
+      it_behaves_like 'the API client'
     end
   end
 end
--- spec/integrations/api_clients/my_pagination_api_client_spec.rb
+++ spec/integrations/api_clients/my_pagination_api_client_spec.rb
@@ -1,16 +1,40 @@
 # frozen_string_literal: true
 
 require './example/api_clients/my_pagination_api_client'
+require 'my_api_client/rspec'
 
 RSpec.describe 'Integration test with My Pagination API', type: :integration do
-  let(:api_client) { MyPaginationApiClient.new }
-
   describe 'GET pagination' do
-    it 'returns an array of posts ordered by id' do
-      pageable_get = api_client.pagination
-      expect(pageable_get.next.page).to eq 1
-      expect(pageable_get.next.page).to eq 2
-      expect(pageable_get.next.page).to eq 3
+    shared_examples 'the API client' do
+      it 'returns an array of posts ordered by id' do
+        pageable_get = api_client.pagination
+        expect(pageable_get.next.page).to eq 1
+        expect(pageable_get.next.page).to eq 2
+        expect(pageable_get.next.page).to eq 3
+      end
+    end
+
+    context 'with real connection' do
+      let(:api_client) { MyPaginationApiClient.new }
+
+      it_behaves_like 'the API client'
+    end
+
+    context 'with stubbed API client' do
+      let(:api_client) do
+        stub_api_client(
+          MyPaginationApiClient,
+          pagination: {
+            pageable: [
+              { page: 1 },
+              { page: 2 },
+              { page: 3 },
+            ],
+          }
+        )
+      end
+
+      it_behaves_like 'the API client'
     end
   end
 end
