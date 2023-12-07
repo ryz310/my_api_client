4756005b9b487183fda65ed2c5341d8f737220f6
Ryosuke Sato <ryz310@gmail.com>
2023-12-07 14:33:09 +0900

    Add specifications about the pagination

--- example/api_clients/my_pagination_api_client.rb
+++ example/api_clients/my_pagination_api_client.rb
@@ -5,9 +5,18 @@ require_relative 'application_api_client'
 # An usage example of the `my_api_client`.
 # See also: my_api/app/controllers/pagination_controller.rb
 class MyPaginationApiClient < ApplicationApiClient
+  # Paging with JSONPath
   # GET pagination?page=1
-  def pagination
-    pget 'pagination', paging: '$.links.next', headers: headers, query: { page: 1 }
+  def paging_with_jsonpath
+    pget 'pagination', headers: headers, query: { page: 1 }, paging: '$.links.next'
+  end
+
+  # Paging with Proc
+  # GET pagination?page=1
+  def paging_with_proc
+    pget 'pagination', headers: headers, query: { page: 1 }, paging: lambda { |response|
+      response.data.links.next
+    }
   end
 
   private
